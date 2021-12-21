fdb0728014cbbf7b1ca25928053544954d9e78c3
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2021-12-21 16:01:54 +0600

    first commit

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -6,7 +6,6 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
     }
     
 }
